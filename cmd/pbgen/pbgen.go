1713e79dad496bae528076b0ecd4d9c667ad8920
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-05-02 17:59:06 +0300

    feat: parse flags

--- cmd/pbgen/pbgen.go
+++ cmd/pbgen/pbgen.go
@@ -1,7 +1,42 @@
 package pbgen
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jxskiss/mcli"
+)
+
+var flags struct {
+	Id      int    `cli:"#R, -i, --id, Problem ID"`
+	Lang    string `cli:"-l, --lang, Programming language for the template [c, cpp, pas]" env:"PB_LANG" default:"cpp"`
+	BaseDir string `cli:"-d, --dir, Base path for project" env:"PB_BASEDIR" default:"."`
+}
+
+const (
+	LANG_C int = iota
+	LANG_CPP
+	LANG_PAS
+)
+
+var langId int
 
 func Run() {
-	fmt.Println("123")
+	app := &mcli.App{
+		Description: "Create a project from a PBInfo statement",
+		Options:     mcli.Options{AllowPosixSTMO: true},
+	}
+	app.SetGlobalFlags(&flags)
+	mcli.Parse(&flags)
+	switch flags.Lang {
+	case "c":
+		langId = LANG_C
+	case "cpp":
+		langId = LANG_CPP
+	case "pas":
+		langId = LANG_PAS
+	default:
+		fmt.Println("Language must be c, cpp or pas!")
+		mcli.PrintHelp()
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/overanalytcl/pbgen
 
 go 1.22.2
 
-require github.com/alecthomas/kong v0.9.0
+require (
+	github.com/MakeNowJust/heredoc/v2 v2.0.1 // indirect
+	github.com/jxskiss/mcli v0.9.5 // indirect
+)
